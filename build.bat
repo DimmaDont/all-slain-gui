6eee84e14e4688bafb92a0a29c56a0c4e7684340
Dimma Don't <93484965+DimmaDont@users.noreply.github.com>
2025-06-01 20:14:34 -0400

    make build command more readable

--- build.bat
+++ build.bat
@@ -1,1 +1,10 @@
-pyinstaller --clean --onefile main.pyw --name allslain_gui --icon icon/ded.ico --noupx --debug noarchive --add-data icon:icon --exclude-module pkg_resources --optimize 2
+pyinstaller ^
+    --clean ^
+    --onefile main.pyw ^
+    --name allslain_gui ^
+    --icon icon/ded.ico ^
+    --noupx ^
+    --debug noarchive ^
+    --add-data icon:icon ^
+    --exclude-module pkg_resources ^
+    --optimize 2
