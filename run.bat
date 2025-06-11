7c0902e64e100c07c94d62314379871c5a32e2d5
Dimma Don't <93484965+DimmaDont@users.noreply.github.com>
2025-06-10 21:29:39 -0400

    fix dependency update

--- run.bat
+++ run.bat
@@ -4,7 +4,7 @@ echo setting up...
 python -m venv %~dp0\venv --upgrade-deps >NUL 2>NUL
 
 echo downloading and installing dependencies...
-%~dp0\venv\Scripts\python.exe -m pip install -U -e %~dp0 >NUL 2>NUL
+%~dp0\venv\Scripts\python.exe -m pip install -U --upgrade-strategy eager -e %~dp0 >NUL 2>NUL
 
 %~dp0\venv\Scripts\python.exe -OO %~dp0\main.pyw
 
